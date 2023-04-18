9f97b1e15db457aa30f37fc8647e96b928737ca7
nicolaazonta <nicolazonta114@gmail.com>
2023-04-18 17:30:22 +0200

    adjust padding

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -15,7 +15,7 @@ export default {
 
 <template>
     <div class="container">
-        <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 g3">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4 py-4">
             <CardItem :card="card" v-for="card in store.cards"/>
                 
         </div>
